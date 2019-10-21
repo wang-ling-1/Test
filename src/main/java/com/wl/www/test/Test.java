60020a97661ca50ed19e8e31d46999e71071e83c
wangling <wl13111196339@126.com>
2019-10-21 18:33:01 +0800

    更改Test类

--- src/main/java/com/wl/www/test/Test.java
+++ src/main/java/com/wl/www/test/Test.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Test {
     public static void main(String[] args) {
         String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        Integer a=01;
+        Integer a=1;
         System.out.println(a);
     }
 }
